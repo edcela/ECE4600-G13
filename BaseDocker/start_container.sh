86f46eea80ca792287f73f52608a9eaf07d751ca
edcela <158010272+edcela@users.noreply.github.com>
2024-12-18 22:29:33 -0600

    Update start_container.sh

--- BaseDocker/start_container.sh
+++ BaseDocker/start_container.sh
@@ -8,6 +8,4 @@ docker run \
     -e DISPLAY \
     -e ROS_DOMAIN_ID=42 \
     -v $(pwd):/workspace \ # This workspace is for already built components if you don't want to colcon buildin uros_ws
-    ece4600-base:2 
-    #udp4 \
-    #--port 8888
+    $2
