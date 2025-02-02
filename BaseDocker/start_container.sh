8e69e7cf243800c6f2e0e330cc1f30b0de7d2380
E <158010272+edcela@users.noreply.github.com>
2025-02-02 13:40:14 -0600

    Update start_container.sh
    The comments are meant to be used when using rqt_graph  and rviz for simulations

--- BaseDocker/start_container.sh
+++ BaseDocker/start_container.sh
@@ -1,11 +1,19 @@
-#!/usr/bin/bash
+#usr/bin/bash
+#xhost +local:docker
 
 docker run \
     -it \
     --rm \
     --name $1 \
     --net=host \
-    -e DISPLAY \
+    -e DISPLAY=$DISPLAY \
     -e ROS_DOMAIN_ID=42 \
-    -v $(pwd):/workspace \ # This workspace is for already built components if you don't want to colcon buildin uros_ws
-    $2
+    -v $(pwd):/workspace \
+    ece4600-base:$2 
+
+# This workspace is for already built components if you don't want to colcon buildin uros_wsece4600-base
+# For RVIZ 
+    #-e DISPLAY=$DISPLAY \
+    #-e QT_X11_NO_MITSHM=1 \
+    #-v /tmp/.X11-unix/tmp/.X11-unix \
+
