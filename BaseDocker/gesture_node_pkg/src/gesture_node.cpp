7c8fab0f2103c553c507096f8677f64bd5b1414c
DRI3NFS <lejanorr@myumanitoba.ca>
2025-02-23 19:12:39 -0600

    First Iteration of ID to Command Logic
    - Added bank of robotic agent command gestures
    - Added bank of robotic agent select gestures
    - Added basic logic for switching between "CONTROL" phase and "SELECT" phase of agent control

--- BaseDocker/gesture_node_pkg/src/gesture_node.cpp
+++ BaseDocker/gesture_node_pkg/src/gesture_node.cpp
@@ -9,7 +9,97 @@
 #include "std_msgs/msg/u_int8_multi_array.hpp"
 #include "Glove.h"
 using namespace std::chrono_literals;  // This will allow us to use time literals like "1s", "500ms", etc.
-                                       
+
+//  Establish Gesture Bank for Robot Commands
+std::vector<gesture> initializeCommandBank(){
+  std::vector<gesture> bank;
+
+  gesture pointUp;            //drone go up
+  gesture pointDown;          //drone go down
+  gesture pointForward;       //drone go forward
+  gesture thumbsBack;         //drone go backward
+  gesture thumbsLeft;         //drone go left
+  gesture thumbsRight;        //drone go right
+  
+  pointUp.setOrientation(FINGER_UP);
+  pointUp.setFingerStates(FLEX,EXTD,FLEX,FLEX,FLEX);
+  pointUp.assignDroneCommand(CMD_ASCEND);
+  bank.push_back(pointUp);
+
+  pointDown.setOrientation(FINGER_DOWN);
+  pointDown.setFingerStates(FLEX,EXTD,FLEX,FLEX,FLEX);
+  pointDown.assignDroneCommand(CMD_DESCEND);
+  bank.push_back(pointDown);
+
+  pointForward.setOrientation(THUMB_UP);
+  pointForward.addOrientation(PALM_UP);
+  pointForward.addOrientation(PALM_DOWN);
+  pointForward.setFingerStates(FLEX,EXTD,FLEX,FLEX,FLEX);
+  pointForward.assignDroneCommand(CMD_FORWARD);
+  bank.push_back(pointForward);
+
+  thumbsBack.setOrientation(FINGER_UP);
+  thumbsBack.setFingerStates(EXTD,FLEX,FLEX,FLEX,FLEX);
+  thumbsBack.assignDroneCommand(CMD_BACK);
+  bank.push_back(thumbsBack);
+
+  thumbsLeft.setOrientation(PALM_DOWN);
+  thumbsLeft.setFingerStates(EXTD,FLEX,FLEX,FLEX,FLEX);
+  thumbsLeft.assignDroneCommand(CMD_LEFT);
+  bank.push_back(thumbsLeft);
+
+  thumbsRight.setOrientation(PALM_UP);
+  thumbsRight.setFingerStates(EXTD,FLEX,FLEX,FLEX,FLEX);
+  thumbsRight.assignDroneCommand(CMD_RIGHT);
+  bank.push_back(thumbsRight);
+  
+  return bank;
+}
+
+//  Establish Gesture Bank for Robot Selection
+std::vector<gesture> initializeSelectBank(){
+  std::vector<gesture> bank;
+
+  gesture oneGesture;
+  gesture twoGesture;
+  gesture threeGesture;
+  gesture fourGesture;
+  gesture thumbsDown;         //reject
+  gesture thumbsUp;           //select
+  
+  oneGesture.setFingerStates(FLEX,EXTD,FLEX,FLEX,FLEX);
+  oneGesture.addFingerStates(FLEX,FLEX,FLEX,FLEX,EXTD);
+  oneGesture.setOrientation(FINGER_UP);
+  oneGesture.setDroneID((uint8_t)1);
+  bank.push_back(oneGesture);
+
+  twoGesture.setFingerStates(FLEX,EXTD,EXTD,FLEX,FLEX);
+  twoGesture.addFingerStates(FLEX,FLEX,FLEX,EXTD,EXTD);
+  twoGesture.setOrientation(FINGER_UP);
+  twoGesture.setDroneID((uint8_t)2);
+  bank.push_back(twoGesture);
+
+  threeGesture.setFingerStates(FLEX, EXTD, EXTD, EXTD, FLEX);
+  threeGesture.addFingerStates(FLEX, FLEX, EXTD, EXTD, EXTD);
+  threeGesture.setOrientation(FINGER_UP);
+  threeGesture.setDroneID((uint8_t)3);
+  bank.push_back(threeGesture);
+
+  fourGesture.setFingerStates(FLEX, EXTD, EXTD, EXTD, EXTD);
+  fourGesture.setOrientation(FINGER_UP);
+  fourGesture.setDroneID((uint8_t)4);
+  bank.push_back(fourGesture);
+
+  thumbsDown.setOrientation(THUMB_DOWN);
+  thumbsDown.setFingerStates(EXTD,FLEX,FLEX,FLEX,FLEX);
+  bank.push_back(thumbsDown);
+  
+  thumbsUp.setOrientation(THUMB_UP);
+  thumbsUp.setFingerStates(EXTD,FLEX,FLEX,FLEX,FLEX);
+  bank.push_back(thumbsUp);
+  
+  return bank;
+}
 
 //  The Node
 class GestureNode : public rclcpp::Node
@@ -17,6 +107,15 @@ class GestureNode : public rclcpp::Node
     public:
         GestureNode() : Node("gesture_node")
         {
+            //Creates bank of gestures
+            static std::vector<gesture> droneGestureBank = initializeCommandBank();
+            static std::vector<gesture> selectGestureBank = initializeSelectBank();
+
+            //Initialize configuration for mode toggle gesture, The gesture is "Rock and Roll"
+            toggle.setOrientation(FINGER_UP);
+            toggle.setFingerStates(FLEX,EXTD,FLEX,FLEX,EXTD);
+            toggle.addFingerStates(EXTD,EXTD,FLEX,FLEX,EXTD);
+          
             //  Subscribe to the Glove Topic 
             subscription_ = this->create_subscription<std_msgs::msg::UInt8>
             (
@@ -44,13 +143,34 @@ class GestureNode : public rclcpp::Node
             //  Access the data from the received message 
             uint8_t data = msg->data;
 
-            //  Determine Command/Agent Here
-            //  CONVERSIONS
-            //
-            //
-            //
-            //
-            //  CONVERSIONS
+            //Toggle between agent select and control mode
+            if(toggle.checkGesture(data)){
+              mode_ = (mode_ == SELECT_MODE) ? CONTROL_MODE : SELECT_MODE;
+            }
+
+            //Select Phase Logic
+            else if(mode_ == PHASE_SELECTION){
+              std::vector<gesture>::iterator it = std::find_if(selectGestureBank.begin(), selectGestureBank.end(), [](const gesture& g){
+                  return g.checkGesture(data);   //equality condition, this checks if there is a matching ID with the gestures in the gesture bank
+              });
+
+              //WIP, FEB 23 (ralph)
+            }
+
+            //Control Phase Logic
+            else if(mode_ == PHASE_CONTROL){
+              std::vector<gesture>::iterator it = std::find_if(droneGestureBank.begin(), droneGestureBank.end(), [](const gesture& g){
+                  return g.checkGesture(data);   //equality condition, this checks if there is a matching ID with the gestures in the gesture bank
+              });
+  
+              if(it != droneGestureBank.end()){
+                latest_cmd_ = (*it).getDroneCommand();
+              }
+  
+              else{  //Default Case
+                latest_cmd_ = CMD_INVALID;
+              }
+            }
 
             //  Log the calculation 
             RCLCPP_INFO(this->get_logger(), "Received: [%u], Result: %d", static_cast<unsigned int>(data), result);
@@ -77,6 +197,12 @@ class GestureNode : public rclcpp::Node
 
         uint8_t latest_agentid_ = 0;
         uint8_t latest_cmd_ = 0;
+
+        uint8_t selectedAgents;             //Buffer for agent select phase
+
+        gesture toggle;                     //Toggle gesture for Select and Control Agent phases
+        phase mode_ = PHASE_SELECTION;      //Indicates what mode you are currently on. "phase" included in Glove.h
+
         int result = 0;
         int status = 0;
 };
