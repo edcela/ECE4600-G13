1afd0f19d00e5b08ad00f74bc4ec12faea72d6ac
DRI3NFS <lejanorr@myumanitoba.ca>
2025-03-11 22:02:22 -0500

    Glove.cpp further concision
    - Simplified glove.cpp A LOT
    - removed all functions involving the use of a single flexion and orientation
    - by default, a gesture will only have a flexion vector and orientation vector
    - Redundancy update

--- BaseDocker/gesture_node_pkg/src/Glove.cpp
+++ BaseDocker/gesture_node_pkg/src/Glove.cpp
@@ -7,12 +7,13 @@
 //Gesture class
 //This class is now repurposed to be a means of categorizing gestures and comparing them to the current gesture
 //This class also includes the command equivalent of the assigned gesture
+//Functions commented out are obselete functions that were programmed for troubleshooting
 gesture::gesture() 
-        :   orientation(0), 
+        :   /*orientation(0), 
             flexion(0), 
             gestureID(0), 
             multipleOrientations(false),
-            multipleFlexionStates(false), 
+            multipleFlexionStates(false), */
             assignedDroneCommand(CMD_INVALID),
             droneID(0)
             {}
@@ -21,46 +22,47 @@ gesture::gesture()
 gesture::~gesture() {}       //deconstructor
 
 void gesture::updateID(uint8_t ID){
-    gestureID = ID;
-    flexion = ID & 0xF8;
-    orientation = ID & 0x07;
+    uint8_t flexion = ID & 0xF8;
+    uint8_t orientation = ID & 0x07;
+
+        allowedFlexionStates.push_back(flexion);
+        allowedOrientations.push_back(orientation);
+
+        if(gesturePriority >= 1){
+                gesturePriority = 0;
+        }
 }
 
 //Get the sensor data of the gesture
-uint8_t gesture::getGestureID() const {
+/*uint8_t gesture::getGestureID() const {
     return gestureID;
-}
+}*/
 
 //Set the BASE orientation to be used for IDing a gesture.
-void gesture::setOrientation(uint8_t inputOrientation){
+/* void gesture::setOrientation(uint8_t inputOrientation){
     gestureID = (gestureID & 0xF8) | inputOrientation;           //clear last 3 bits
     orientation = inputOrientation;
         if(gesturePriority >= 1){
                 gesturePriority = 0;
         }
-}
+} */
 
 //If a gesture is valid for multiple SPECIFIC BUT NOT ALL orientations, add them using this function.
 void gesture::addOrientation(uint8_t inputOrientation){
         if(gesturePriority >= 1){
                 gesturePriority = 0;
         }
-    if(!multipleOrientations){
-        multipleOrientations = true;
-        allowedOrientations = {orientation, inputOrientation};
-    }
-    else{
+        
         allowedOrientations.push_back(inputOrientation);
-    }
 }
 
 //Returns the orientation of the gesture. If multiple orientations are allowed, only the first orientation is returned.
-uint8_t gesture::getOrientation() const {
+/*uint8_t gesture::getOrientation() const {
     return orientation;
-}
+}*/
 
 //Set the flexion states of the fingers for this gesture. The order is thumb, index, middle, ring, pinky.
-void gesture::setFingerStates(uint8_t thumb, uint8_t index, uint8_t middle, uint8_t ring, uint8_t pinky) {
+/*void gesture::setFingerStates(uint8_t thumb, uint8_t index, uint8_t middle, uint8_t ring, uint8_t pinky) {
     uint8_t fingerStateBits = 0;
     fingerStateBits |= (index << 7);
     fingerStateBits |= (middle << 6);
@@ -70,7 +72,7 @@ void gesture::setFingerStates(uint8_t thumb, uint8_t index, uint8_t middle, uint
 
     gestureID = (gestureID & 0x07) | fingerStateBits;  // Replace the first 5 bits with the new finger states
     flexion = fingerStateBits;
-}
+}*/
 
 //If a gesture requires multiple possible finger states, add them using this function.
 void gesture::addFingerStates(uint8_t thumb, uint8_t index, uint8_t middle, uint8_t ring, uint8_t pinky){
@@ -80,19 +82,14 @@ void gesture::addFingerStates(uint8_t thumb, uint8_t index, uint8_t middle, uint
     fingerStateBits |= (ring << 5);
     fingerStateBits |= (pinky << 4);
     fingerStateBits |= (thumb << 3);
-
-    if (!multipleFlexionStates) {
-        multipleFlexionStates = true;
-        allowedFlexionStates = {flexion, fingerStateBits};
-    } else {
+        
         allowedFlexionStates.push_back(fingerStateBits);
-    }
 }
 
 //Returns flexion states. As in the gestureID of this gesture with orientation omitted.
-uint8_t gesture::getFingerStates() const {
+/*uint8_t gesture::getFingerStates() const {
     return flexion;
-}
+}*/
 
 void gesture::assignDroneCommand(DroneCommand command){
     assignedDroneCommand = command;
@@ -117,12 +114,10 @@ bool gesture::checkGesture(uint8_t currentGesture) const {
     //a gesture only has one allowed state.
 
     //Case 1: Any orientation is valid, only finger states are checked
-    if (orientation == 0) {
-        return !multipleFlexionStates
-            ? (currentGesture & 0xF8) == flexion
-            : std::find(allowedFlexionStates.begin(), allowedFlexionStates.end(), (currentGesture & 0xF8)) != allowedFlexionStates.end();
+    if (allowedOrientations.empty()) {
+        return std::any_of(allowedFlexionStates.begin(), allowedFlexionStates.end(), [currentGesture](uint8_t flexion){return (currentGesture&0xF8) == flexion;});
     }
-
+/*
     //Case 2: Only one finger state is valid for this gesture
     if (!multipleFlexionStates) {
         if (!multipleOrientations) {
@@ -137,11 +132,10 @@ bool gesture::checkGesture(uint8_t currentGesture) const {
         return std::any_of(allowedFlexionStates.begin(), allowedFlexionStates.end(),
             [currentGesture, this](uint8_t flexion) { return currentGesture == (flexion + orientation); });
     }
-    
-    //Case 4: Multiple finger states and multiple orientations are valid for this gesture
+*/
+    //Case 2: Multiple finger states and multiple orientations are valid for this gesture
     return std::any_of(allowedFlexionStates.begin(), allowedFlexionStates.end(),
-        [this, currentGesture](uint8_t flexion) {
-            return std::any_of(allowedOrientations.begin(), allowedOrientations.end(),
+        [currentGesture](uint8_t flexion) { return std::any_of(allowedOrientations.begin(), allowedOrientations.end(),
                 [currentGesture, flexion](uint8_t orientation) { return currentGesture == (flexion + orientation); });
         });
 }
@@ -158,14 +152,6 @@ bool gesture::operator > (const gesture& otherGesture) const {
 bool gesture::operator == (const gesture& otherGesture) const {
     return checkGesture(otherGesture.getGestureID());
 }
-
-/*
-void gesture::operator = (gesture& replacementGesture){
-    gestureID = replacementGesture.getGestureID();
-    flexion = replacementGesture.getFingerStates();
-    orientation = replacementGesture.getOrientation();
-}
-*/
 
 //removes all gesture information
 void gesture::clearGesture(){
